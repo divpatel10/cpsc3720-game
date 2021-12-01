b94f00bc7f8a9e18d93366048c69f86d926c503a
killerox15 <80929445+killerox15@users.noreply.github.com>
2021-12-01 14:50:10 -0500

    Changed game to loop
    Changed the game to loop until the player types "quit", allowing for generating multiple responses.

--- gamefile.py
+++ gamefile.py
@@ -4,18 +4,22 @@ import json
 
 print("Welcome to Animal Charades, where you act out an animal doing a certain activity!")
 verb = ["running", "walking", "sleeping", "dancing", "jumping", "singing", "swimming", "triping", "fighting", "crying", "laughing", "shouting", "cycling", "driving", "eating", "programming", "exercising", "cooking"]
+while(True):
+    animal_response = requests.get("https://random-word-form.herokuapp.com/random/animal")
 
-animal_response = requests.get("https://random-word-form.herokuapp.com/random/animal")
+    # print(animal_response.json())
 
-# print(animal_response.json())
+    animal = animal_response.json()[0]
 
-animal = animal_response.json()[0]
+    max_num = len(verb) -1
+    number_response = requests.get( f"http://www.randomnumberapi.com/api/v1.0/random?max={max_num}");
 
-max_num = len(verb) -1
-number_response = requests.get( f"http://www.randomnumberapi.com/api/v1.0/random?max={max_num}");
+    number = number_response.json()[0]
 
-number = number_response.json()[0]
+    # print(number_response.json())
 
-# print(number_response.json())
+    print(f"Your animal and its activty is: A {verb[number]} {animal}\n\nPress Enter to generate another or type quit to end the game")
 
-print(f"Your animal and its activty is: A {verb[number]} {animal}")
+    playerInput = input()
+    if(playerInput == "quit"):
+        break
